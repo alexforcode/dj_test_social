12ec32e1ce8f389a1e7a27659f5d8cf9dd8d792b
Alex Code <alex.for.code@gmail.com>
2020-04-28 22:54:34 +0300

    Add password change views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-change/',
+         PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')),
+         name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
