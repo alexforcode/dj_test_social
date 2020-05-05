3185165df34b763f638615ea2b62d724d0006611
Alex Code <alex.for.code@gmail.com>
2020-05-06 00:25:30 +0300

    Add users follow/unfollow functionality.

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -19,6 +19,8 @@ class Contact(models.Model):
     user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
+    objects = models.Manager()
+
     def __str__(self):
         return f'{self.user_from} follows {self.user_to}'
 
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
 from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
-from .models import Profile
+from .models import Profile, Contact
+from common.decorators import ajax_required
 
 
 @login_required
@@ -77,3 +80,24 @@ def user_detail(request, username):
         'user': user
     }
     return render(request, 'account/user/detail.html', context)
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
